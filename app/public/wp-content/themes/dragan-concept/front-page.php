530de43c39c83f6996044a7b8bca9bcbb07a028a
Dragan Trojanovic <dragan.t@orange.fr>
2023-08-28 20:04:57 +0200

    Animations page accueil

--- app/public/wp-content/themes/dragan-concept/front-page.php
+++ app/public/wp-content/themes/dragan-concept/front-page.php
@@ -11,14 +11,20 @@ get_header();
 <section class="hero-section">
     <div class="hero-content">
         <div class="hero-text">
-            <h1>Développeur Intégrateur Web junior</h1>
-            <p>Spécialisé WordPress</p>
-            <p>Création de site web<br> sur-mesure</p>
+            <h1 class="fade-in">Développeur Intégrateur Web junior</h1>
+            <p class="fade-in">Spécialisé WordPress</p>
+            <p class="fade-in">Création de site web<br> sur-mesure</p>
         </div>
         <div class="hero-image">
         <img class="man-laptop" src="<?php echo esc_url( home_url( '/wp-content/uploads/2023/08/homme-assis-ordinateur.png' ) ); ?>" alt="Homme assis devant un ordinateur portable">
         </div>
-    </div>
+        <div id="container">
+            <div class="steam" id="steam1"> </div>
+            <div class="steam" id="steam2"> </div>
+            <div class="steam" id="steam3"> </div>
+            <div class="steam" id="steam4"> </div>
+        </div>
+
         <div class="container">
         <div class="mouse">
 		    <span></span>
@@ -26,7 +32,7 @@ get_header();
         </div>
 </section>
 
-<section class="about-section fade-in-up-about">
+<section class="about-section fade-in-up-about hidden">
     <div class="about-heading">
         <h2>À propos</h2>
     </div>
@@ -56,16 +62,17 @@ get_header();
          $projects->the_post();
          $image = get_field('image');
          $title = get_the_title();
+         $techno = get_field('techno');
          $description = get_field('description');
          $permalink = get_permalink();
    ?>
    
-   <div class="portfolio-card">
+   <div class="portfolio-card fade-in-up-card hidden">
       <a href="<?php echo esc_url($permalink); ?>">
          <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($title); ?>">
          <div class="portfolio-card-overlay">
             <h3><?php echo esc_html($title); ?></h3>
-            <p><?php echo esc_html($description); ?></p>
+            <p><?php echo esc_html($techno); ?></p>
          </div>
       </a>
    </div>
--- app/public/wp-content/themes/dragan-concept/js/script.js
+++ app/public/wp-content/themes/dragan-concept/js/script.js
@@ -1,15 +1,28 @@
 jQuery(document).ready(function($) {
-    // Creating the IntersectionObserver
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('inview');
-        } else {
-          entry.target.classList.remove('inview');
-        }
-      });
+  const elements = document.querySelectorAll('.animate-from-right, .animate-from-left, .floating-cat, .fade-in-up-about, .fade-in-up-card');
+  
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && entry.intersectionRatio >= 0.5) {
+        entry.target.classList.add('inview');
+        observer.unobserve(entry.target); // Cesser d'observer une fois que l'élément est dans la vue
+      } else {
+        entry.target.classList.remove('inview');
+      }
     });
-    // Observing the elements
-    const elements = document.querySelectorAll('.animate-from-right, .animate-from-left, .floating-cat, .fade-in-up-about');
-    elements.forEach(element => observer.observe(element));
-    });
+  }, { threshold: 0.5 }); // Définir le seuil à 0.5 (50%)
+
+  elements.forEach(element => {
+    observer.observe(element);
+  });
+});
+
+
+
+jQuery(document).ready(function($) {
+  const heroText = document.querySelector('.hero-text');
+
+  if (heroText) {
+    heroText.classList.add('inview');
+  }
+});
